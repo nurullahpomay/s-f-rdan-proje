bc1b8bb5965c2896665ab8af1db5811a5c0ed182
pomay <pomay@DESKTOP-GLR4MKL>
2021-06-21 01:40:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace sıfırdan_proje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string katagori = "katagori";
+            bool sistemeGirisYapmısmı = false;
+            ;
+            if (sistemeGirisYapmısmı== true)
+
+            {
+                Console.WriteLine("kullanıcı ayarlar programı");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+        }
+
+
+
+
+
+        // fonksiyonlar kodu tekrar tekrar kullanmayı sağlar
+        // alias --> deger tutucu = katagori denen şey
+        // do not repeat yourself--> kendini tekrar etme
+        // type safety --> tip güvenliği
+        // pascal case --> kelimelerin başharfleri büyük olur
+        //türkiyede en çok kukllanılır backend teknolojilerinde kullanılır en çok oyun teknolojilerinde kulanım   
+
+         
+    }
+  
+}
